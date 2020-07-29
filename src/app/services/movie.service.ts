9abf214d96b0691b6c28b1d313ec5a00e2949bdc
unknown <bylasuhaimi@gmail.com>
2020-07-29 22:58:47 +0800

    first commit

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+ 
+// Typescript custom enum for search types (optional)
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  url = 'http://www.omdbapi.com/';
+  apiKey = ''; // <-- Enter your own key here!
+ 
+  /**
+   * Constructor of the Service with Dependency Injection
+   * @param http The standard Angular HttpClient to make requests
+   */
+  constructor(private http: HttpClient) { }
+ 
+  /**
+  * Get data from the OmdbApi 
+  * map the result to return only the results that we need
+  * 
+  * @param {string} title Search Term
+  * @param {SearchType} type movie, series, episode or empty
+  * @returns Observable with the search results
+  */
+
+//searchData(): This function searches for results to a specific
+//title and search type – an enum we defined upfront to
+//represent the types that we can pass to the API using TypeScript!
+
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
+      map(results => results['Search'])
+    );
+  }
+ 
+  /**
+  * Get the detailed information for an ID using the "i" parameter
+  * 
+  * @param {string} id imdbID to retrieve information
+  * @returns Observable with detailed information
+  */
+
+  //getDetails(): This function returns the detailed information
+  //for one specific element, will be used on our details page
+  getDetails(id) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
+}
